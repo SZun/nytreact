9380ac1ae3e9f4984be635d7e3f303fb448e72ab
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-28 02:13:57 -0500

    working on proxy

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-import connection from './startup/connection';
-import bodyParser from './startup/bodyParser';
-import articleRoutes from './startup/routes';
+import Connection from './startup/connection';
+import BodyParser from './startup/bodyParser';
+import ArticleRoutes from './startup/routes';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-connection(mongoose);
-bodyParser(app);
-articleRoutes(app);
+Connection(mongoose);
+BodyParser(app);
+ArticleRoutes(app);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
