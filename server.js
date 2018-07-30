ba4a8bcfd3c990e2e16da13ccac07946bceed29a
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-29 19:08:37 -0500

    changing file structure for deployment

--- server.js
+++ server.js
@@ -16,4 +16,17 @@ BodyParser(app);
 ArticleRoutes(app);
 react(app, express);
 
+try {
+  app.use(express.static('client/build'));
+  app.get('*', function(req, res) {
+    if (process.env.NODE_ENV === 'production') {
+      res.sendFile(__dirname + '/client/build/index.html');
+    } else {
+      res.sendFile(__dirname + '/../client/public/index.html');
+    }
+  });
+} catch (err) {
+  console.log(`Error: ${err.message}`);
+}
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
