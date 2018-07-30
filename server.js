cadc767587ff9718b0e92bf9cd3f47498af0a818
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-29 19:18:37 -0500

    refactoring server.js

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ import mongoose from 'mongoose';
 import Connection from './startup/connection';
 import BodyParser from './startup/bodyParser';
 import ArticleRoutes from './startup/routes';
-import Cors from './startup/middleware';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 Connection(mongoose);
 BodyParser(app);
-// Cors(app);
 ArticleRoutes(app);
 
 try {
   app.use(express.static('client/build'));
   app.get('*', function(req, res) {
     if (process.env.NODE_ENV === 'production') {
-      res.sendFile(__dirname + '/client/build/index.html');
+      res.sendFile(__dirname + '/client/build/static/index.html');
     } else {
       res.sendFile(__dirname + '/../client/public/index.html');
     }
